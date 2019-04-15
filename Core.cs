f805638a9b7c330ca6b2e0504e27630bc0e69c3f
Enl1ghted <43586422+Enl1ghted@users.noreply.github.com>
2019-04-15 15:21:57 +0300

    Update Core.cs

--- Core.cs
+++ Core.cs
@@ -3,7 +3,6 @@ using Kernel.Nexus;
 namespace Kernel {
     class Core {
         static void Main(string[] args) {
-            //- Nice -//
             Implementation.Initialization();
             Binder.RequestHandler.Request();
         }
